2d01b371b226763f971276e2bf5cb1473b7ad685
Seb James <seb.james@sheffield.ac.uk>
2021-06-11 11:55:57 +0100

    Print errors in text on top of colour map

--- scripts/plotset.py
+++ scripts/plotset.py
@@ -1,32 +1,19 @@
 import numpy as np
-
 import sebcolour
 C = sebcolour.Colour
 
-# Set plotting font defaults
 import matplotlib
 fs = 18
 fnt = {'family' : 'Arial',
        'weight' : 'regular',
        'size'   : fs}
-matplotlib.rc('font', **fnt)
+matplotlib.rc('font', **fnt) # set default font
 
-# Important for svg output of text as 'things that can be edited in inkscape'
 import pylab as pl
-pl.rcParams['svg.fonttype'] = 'none'
-
-# Control overall figure size here
+pl.rcParams['svg.fonttype'] = 'none' # for good svg output
 fig = pl.figure(figsize=(12,5))
 
-import h5py
 import json
-
-models = ['explog', 'linear', 'expexp']
-expts = ['wildtype', 'tecswap', 'tecrot90', 'tecrot180',
-         'tecablate', 'single', 'retablate',
-         'reber', 'mismatch', 'knockout1',
-         'knockin1', 'compound']
-
 jf = open('runset.json')
 jconf = json.load(jf)
 models = jconf['models']
@@ -35,11 +22,12 @@ expts = jconf['expts']
 # a models (rows) by expts (cols) array for results
 r = np.zeros((len(models),len(expts)))
 
+import h5py
 for mi in range(0,len(models)):
     for ei in range(0,len(expts)):
-        filename = '../log/agent/'+models[mi]+'_'+expts[ei]+'.h5'
+        filename = './log/agent/'+models[mi]+'_'+expts[ei]+'.h5'
         with h5py.File (filename, 'r') as f:
-            r[mi][ei] = list(f['/sos'])[0]
+            r[mi][ei] = list(f['/rms'])[0]
 
 print ('r: {0}'.format(r))
 
@@ -47,15 +35,20 @@ print ('r: {0}'.format(r))
 r_n = r / r.max(axis=0)
 
 ax = fig.add_subplot (1,1,1);
-im = ax.imshow (r, cmap='plasma')
+im = ax.imshow (r, cmap='jet')
 ax.set_xticks(range(0, len(expts)))
 ax.set_yticks(range(0, len(models)))
 ax.set_xticklabels (expts)
 ax.set_yticklabels (models)
 pl.xticks(rotation=90)
 pl.colorbar(im)
-ax.set_title('SOS pattern match. Purple is best. Raw.')
+ax.set_title('RMS error. Low is good. Raw.')
+for mi in range(0,len(models)):
+    for ei in range(0,len(expts)):
+        ax.text (ei-0.25,mi,'{0:.3f}'.format(r[mi][ei]), fontsize=12)
 
+rs = np.sum(r, axis=1)
+print ('rs: {0}'.format (rs))
 
 #pl.tight_layout()
 #pl.savefig('somefile.svg', transparent=True)
