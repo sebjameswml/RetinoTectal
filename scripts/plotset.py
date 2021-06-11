8f4fabf6d77973e969021988b38814bf22854ddd
Seb James <seb.james@sheffield.ac.uk>
2021-06-11 11:02:33 +0100

    Plotting WIP

--- scripts/plotset.py
+++ scripts/plotset.py
@@ -16,15 +16,21 @@ import pylab as pl
 pl.rcParams['svg.fonttype'] = 'none'
 
 # Control overall figure size here
-fig = pl.figure(figsize=(8,10))
+fig = pl.figure(figsize=(12,5))
 
 import h5py
+import json
 
 models = ['explog', 'linear', 'expexp']
 expts = ['wildtype', 'tecswap', 'tecrot90', 'tecrot180',
          'tecablate', 'single', 'retablate',
          'reber', 'mismatch', 'knockout1',
          'knockin1', 'compound']
+
+jf = open('runset.json')
+jconf = json.load(jf)
+models = jconf['models']
+expts = jconf['expts']
 
 # a models (rows) by expts (cols) array for results
 r = np.zeros((len(models),len(expts)))
@@ -37,8 +43,19 @@ for mi in range(0,len(models)):
 
 print ('r: {0}'.format(r))
 
+# Normalise each col of r to show best model for each condition?
+r_n = r / r.max(axis=0)
+
 ax = fig.add_subplot (1,1,1);
-im = ax.imshow (r)
+im = ax.imshow (r, cmap='plasma')
+ax.set_xticks(range(0, len(expts)))
+ax.set_yticks(range(0, len(models)))
+ax.set_xticklabels (expts)
+ax.set_yticklabels (models)
+pl.xticks(rotation=90)
+pl.colorbar(im)
+ax.set_title('SOS pattern match. Purple is best. Raw.')
+
 
 #pl.tight_layout()
 #pl.savefig('somefile.svg', transparent=True)
--- scripts/runset.py
+++ scripts/runset.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
 
+import json
 import subprocess
 
-# Run a set of sims
-models = ['el', 'lin', 'ee']
-expts = ['wt', 'tecswap', 'tecrot90', 'tecrot180',
-         'tecablate', 'single', 'retablate',
-         'reber', 'mismatch', 'knockout1',
-         'knockin1', 'compound']
+# Run a set of sims with models and expts specified in json
+jf = open('runset.json')
+data = json.load(jf)
 
-for m in models:
-    for e in expts:
+for m in data['models']:
+    for e in data['expts']:
         print ("Running Model {0}, Expt {1}".format(m, e))
-        subprocess.call(["./build/sim/agent/agent1c",
-                         "configs/a1/m_{0}.json".format(m),
-                         "configs/a1/e_{0}.json".format(e)])
+        subprocess.call(["../build/sim/agent/agent1c",
+                         "../configs/a1/m_{0}.json".format(m),
+                         "../configs/a1/e_{0}.json".format(e)])
