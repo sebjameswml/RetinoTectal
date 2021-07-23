6a11582ccce8e33c435f81cb1dcfa5224b78eac5
Seb James <seb.james@sheffield.ac.uk>
2021-07-23 16:05:59 +0100

    Splurging out results figures into the draft paper

---
+++ scripts/fig_chemo_surgical1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Rotational wildtype, but with different selected axons:
+./build/sim/agent/agent1 ./configs/a1/m_ee_chemoonly.json ./configs/a1/e_wt_rotcmp.json
+
+./build/sim/agent/agent1 ./configs/a1/m_ee_chemoonly.json ./configs/a1/e_tecrot90.json
+
+./build/sim/agent/agent1 ./configs/a1/m_ee_chemoonly.json ./configs/a1/e_tecrot180.json
+
+./build/sim/agent/agent1 ./configs/a1/m_ee_chemoonly.json ./configs/a1/e_tecswap.json
---
+++ scripts/fig_surgical2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Rotational wildtype, but with different selected axons:
+./build/sim/agent/agent1 ./configs/a1/m_ee.json ./configs/a1/e_retablate.json
+
+./build/sim/agent/agent1 ./configs/a1/m_ee.json ./configs/a1/e_tecablate.json
+
+./build/sim/agent/agent1 ./configs/a1/m_ee.json ./configs/a1/e_mismatch.json
+
+./build/sim/agent/agent1 ./configs/a1/m_ee.json ./configs/a1/e_compound.json
