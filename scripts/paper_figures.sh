1347f26a87bb4bb68dd51d47544820d97b414a31
Seb James <seb.james@sheffield.ac.uk>
2022-02-01 09:23:38 +0000

    Add the 'best model' calls to paper_figures.sh

--- scripts/paper_figures.sh
+++ scripts/paper_figures.sh
@@ -24,3 +24,8 @@
 
 # GJ model, Surgical manipulations
 #./scripts/fig_GJ_surgical.sh
+
+# The 'best model' figures
+./build/sim/agent/agent1 configs/simpler/m_ee_GC_best_1.json configs/simpler/e_wt_figcomp1.json
+./build/sim/agent/agent1 configs/simpler/m_ee_GI_best_1.json configs/simpler/e_wt_figcomp2.json
+./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json configs/simpler/e_wt_figcomp3.json
