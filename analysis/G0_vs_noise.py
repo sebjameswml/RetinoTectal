8c24ec33390cc003335c877300388bc43cee7126
Seb James <seb.james@sheffield.ac.uk>
2022-02-08 18:29:49 +0000

    Plots graph directly from .h5 output from sims (fig_G0_noise.sh)

--- analysis/G0_vs_noise.py
+++ analysis/G0_vs_noise.py
@@ -22,7 +22,6 @@ pl.rcParams['svg.fonttype'] = 'none'
 
 
 # Will need to extract data from .h5 files:
-'''
 files=['../log/agent/eE_G_wt_figcomp2_exit_true_noise_gain_0.0_steps_1000.h5',
 '../log/agent/eE_G_wt_figcomp2_exit_true_noise_gain_0.2_steps_1000.h5',
 '../log/agent/eE_G_wt_figcomp2_exit_true_noise_gain_0.4_steps_1000.h5',
@@ -31,61 +30,44 @@ files=['../log/agent/eE_G_wt_figcomp2_exit_true_noise_gain_0.0_steps_1000.h5',
 '../log/agent/eE_G_wt_figcomp2_exit_true_noise_gain_1.0_steps_1000.h5',
 '../log/agent/eE_G_wt_figcomp2_exit_true_noise_gain_1.5_steps_1000.h5',
 '../log/agent/eE_G_wt_figcomp2_exit_true_noise_gain_2.0_steps_1000.h5',]
-'''
 
-files=['../log/agent/eE_G_wt_figcomp2_exit_true_noise_gain_0.0_steps_1000.h5']
+noise_gain = [0,.2,.4,.6,.8,1,1.5,2]
 
-# Containers for means
+#files=['../log/agent/eE_G_wt_figcomp2_exit_true_noise_gain_0.0_steps_1000.h5']
+
+mean_eta = []
+mean_eps = []
 import h5py
 for fn in files:
     with h5py.File (fn, 'r') as f:
-        print("Keys: {0}".format(list(f.keys())))
-        # It's a bit ugly, the way you get the value of the nframes
-        # object; you have to convert the hdf5 object into a list, then
-        # get the 0th value of the list (which always has one element)
-        crosscount = list(f['/crosscount'])
+        t = np.array(list(f['/t']))
+        crosscount = np.array(list(f['/crosscount']))
+
+        cc_idx = np.nonzero(np.where (crosscount > -1, 1, 0)) # incantation to make an index array
+        cc_red = crosscount[cc_idx]
+        t_red = t[cc_idx]
         rms = list(f['/crosscount'])
-        t = list(f['/t'])
-        # Matlab: c = crosscount(crosscount~=-1)
-        print ('crosscount: {0}'.format(crosscount))
-        cc_where = np.where(crosscount != -1)
-        print ('t where {0}'.format(cc_where))
+        # 500-1000 time slice is [41:] 750-1000 slice is [66:]
+        mean_eta.append(np.mean(cc_red[66:]))
+        # 500-1000 time slice is [100:] 750-1000 slice is [150:]
+        mean_eps.append(np.mean(rms[150:]))
 
+fig, ax1 = plt.subplots()
 
-'''
-with open ('GI_vs_noise.txt') as csvfile:
-    rdr = csv.reader (csvfile, delimiter=',')
-    first = True
-    noise_gain = []
-    mean_eta = []
-    mean_eps = []
-    for row in rdr:
-        if first:
-            # Don't process header
-            first = False
-        else:
-            print ('noise_gain: {0} mean_eta {1}, mean_eps {2}'.format (row[0], row[1], row[2]))
-            noise_gain.append(float(row[0]))
-            mean_eta.append(float(row[1]))
-            mean_eps.append(float(row[2]))
+color = clr2
+ax1.set_xlabel('Noise gain')
+# The bar reproduces well in matplotlib, but not in svg/inkscape
+#ax1.set_ylabel(r'$\bar{\eta}$', rotation=0,  color=blk)
+ax1.set_ylabel(r'$\eta$', rotation=0,  color=blk)
+ax1.plot(noise_gain, mean_eta, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
 
-    fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+color = clr1
+ax2.set_ylabel(r'$\epsilon$', rotation=0, color=blk)  # we already handled the x-label with ax1
+ax2.plot(noise_gain, mean_eps, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
 
-    color = clr2
-    ax1.set_xlabel('Noise gain')
-    # The bar reproduces well in matplotlib, but not in svg/inkscape
-    #ax1.set_ylabel(r'$\bar{\eta}$', rotation=0,  color=blk)
-    ax1.set_ylabel(r'$\eta$', rotation=0,  color=blk)
-    ax1.plot(noise_gain, mean_eta, color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
-
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    color = clr1
-    ax2.set_ylabel(r'$\epsilon$', rotation=0, color=blk)  # we already handled the x-label with ax1
-    ax2.plot(noise_gain, mean_eps, color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
-
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
-    plt.savefig('GI_vs_noise.svg', transparent=True)
-    plt.show()
-'''
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.savefig('G0_vs_noise.svg', transparent=True)
+plt.show()
