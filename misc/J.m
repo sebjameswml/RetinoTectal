375353a8941ba05c8b673ee1c8e946041a22142b
Seb James <seb.james@sheffield.ac.uk>
2022-02-10 10:13:05 +0000

    Shows the combined repulsion interaction for GJ model for 1 axis

--- misc/J.m
+++ misc/J.m
@@ -20,21 +20,57 @@ _exp2_ki = _exp2 + ki;
 _exp2_ki_kd = _exp2_ki - kd;
 _exp2_kd = _exp2 - kd;
 
-% The J effect
+%% The J effect
+_r0 = _exp;
+_l0 = flip(_exp);
 figure(1); clf;
-plot (x, _exp); % exp for receptors on retina
+plot (x, _r0); % exp for receptors on retina
 hold on;
-_lig = flip(_exp);
-plot (x, _lig); % ligands
-plot (x, _exp .* _lig, 'color', colour.orchid2, 'linestyle', '--');
-plot (x(1:3), _exp(1) .* _lig(1:3), 'ro-');
-plot ([x(1),x(1)], [_exp(2),_exp(1).*_lig(1)], 'ro-');
-plot (x(3:7), _exp(5) .* _lig(3:7), 'b');
-plot ([x(5),x(5)], [_exp(5),_exp(5).*_lig(5)], 'b-');
-plot (x(7:11), _exp(9) .* _lig(7:11), 'g');
-plot ([x(9),x(9)], [_exp(9),_exp(9).*_lig(9)], 'g-');
-plot (x(11:15), _exp(13) .* _lig(11:15), 'm');
-plot ([x(13),x(13)], [_exp(13),_exp(13).*_lig(13)], 'm-');
-plot (x(15:19), _exp(17) .* _lig(15:19), 'k');
-plot ([x(17),x(17)], [_exp(17),_exp(17).*_lig(17)], 'k-');
-lg = legend (['rcpt';'lgnd'],'Location','North');
+plot (x, _l0); % ligands
+plot (x, _r0 .* _l0, 'color', colour.orchid2, 'linestyle', '--');
+plot (x(1:3), _r0(1) .* _l0(1:3), 'ro-');
+plot ([x(1),x(1)], [_r0(2),_r0(1).*_l0(1)], 'ro-');
+plot (x(3:7), _r0(5) .* _l0(3:7), 'b');
+plot ([x(5),x(5)], [_r0(5),_r0(5).*_l0(5)], 'b-');
+plot (x(7:11), _r0(9) .* _l0(7:11), 'g');
+plot ([x(9),x(9)], [_r0(9),_r0(9).*_l0(9)], 'g-');
+plot (x(11:15), _r0(13) .* _l0(11:15), 'm');
+plot ([x(13),x(13)], [_r0(13),_r0(13).*_l0(13)], 'm-');
+plot (x(15:19), _r0(17) .* _l0(15:19), 'k');
+plot ([x(17),x(17)], [_r0(17),_r0(17).*_l0(17)], 'k-');
+lg = legend (['rcpt0';'lgnd0'],'Location','North');
+
+_r1 = flip(_exp); % opposing receptors
+_l1 = _exp; % opposing ligands
+figure(2); clf;
+plot (x, _r1);
+hold on;
+plot (x, _l1); % ligands
+plot (x, _r1 .* _l1, 'color', colour.orchid2, 'linestyle', '--');
+plot (x(1:3), _r1(1) .* _l1(1:3), 'ro-');
+plot ([x(1),x(1)], [_r1(2),_r1(1).*_l1(1)], 'ro-');
+plot (x(3:7), _r1(5) .* _l1(3:7), 'b');
+plot ([x(5),x(5)], [_r1(5),_r1(5).*_l1(5)], 'b-');
+plot (x(7:11), _r1(9) .* _l1(7:11), 'g');
+plot ([x(9),x(9)], [_r1(9),_r1(9).*_l1(9)], 'g-');
+plot (x(11:15), _r1(13) .* _l1(11:15), 'm');
+plot ([x(13),x(13)], [_r1(13),_r1(13).*_l1(13)], 'm-');
+plot (x(15:19), _r1(17) .* _l1(15:19), 'k');
+plot ([x(17),x(17)], [_r1(17),_r1(17).*_l1(17)], 'k-');
+lg = legend (['rcpt1';'lgnd1'],'Location','North');
+
+%% Now plot combined repulsion due to both receptor/ligand pairs
+figure(1)
+plot (x, _r1(1) .* _l1 + _r0(1) .* _l0, 'r--');
+plot (x, _r1(5) .* _l1 + _r0(5) .* _l0, 'b--');
+plot (x, _r1(9) .* _l1 + _r0(9) .* _l0, 'g--');
+plot (x, _r1(13) .* _l1 + _r0(13) .* _l0, 'm--');
+plot (x, _r1(17) .* _l1 + _r0(17) .* _l0, 'k--');
+ylim([0,20])
+figure(2)
+plot (x(1:3), _r1(1) .* _l1(1:3) + _r0(1) .* _l0(1:3), 'r--');
+plot (x(3:7), _r1(5) .* _l1(3:7) + _r0(5) .* _l0(3:7), 'b--');
+plot (x(7:11), _r1(9) .* _l1(7:11) + _r0(9) .* _l0(7:11), 'g--');
+plot (x(11:15), _r1(13) .* _l1(11:15) + _r0(13) .* _l0(11:15), 'm--');
+plot (x(15:19), _r1(17) .* _l1(15:19) + _r0(17) .* _l0(15:19), 'k--');
+ylim([0,20])
