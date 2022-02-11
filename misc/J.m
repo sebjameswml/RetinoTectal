aec0d133b77f0d8ca1704855e6ac2c82c7930669
Seb James <seb.james@sheffield.ac.uk>
2022-02-11 11:54:27 +0000

    Completes 3 possible sub-figures to go in a row

--- misc/J.m
+++ misc/J.m
@@ -46,49 +46,64 @@ plot ([x(21),x(21)], [_r0(21),_r0(21).*_l0(21)], 'ko-');
 lg = legend (['rcpt0';'lgnd0'],'Location','North');
 
 _r2 = _exp; % opposing ligands
-_l1 = flip(_exp); % opposing receptors
+_l2 = flip(_exp); % opposing receptors
 figure(2); clf;
 plot (x, _r2, 'linestyle', ':');
 hold on;
-plot (x, _l1, 'linestyle', ':'); % ligands
-%%plot (x, _r2 .* _l1, 'color', colour.orchid2, 'linestyle', ':');
-%%plot (x(1:3), _r2(1) .* _l1(1:3), 'r-');
-plot (x, _r2(1) .* _l1, 'r');
-plot ([x(1),x(1)], [_r2(2),_r2(1).*_l1(1)], 'ro-');
-plot (x(3:7), _r2(5) .* _l1(3:7), 'b');
-plot ([x(5),x(5)], [_r2(5),_r2(5).*_l1(5)], 'bo-');
-plot (x(7:11), _r2(9) .* _l1(7:11), 'g');
-plot ([x(9),x(9)], [_r2(9),_r2(9).*_l1(9)], 'go-');
-plot (x(11:15), _r2(13) .* _l1(11:15), 'm');
-plot ([x(13),x(13)], [_r2(13),_r2(13).*_l1(13)], 'mo-');
-plot (x(15:19), _r2(17) .* _l1(15:19), 'k');
-plot (x, _r2(17) .* _l1, 'k');
-plot ([x(17),x(17)], [_r2(17),_r2(17).*_l1(17)], 'ko-');
+plot (x, _l2, 'linestyle', ':'); % ligands
+%%plot (x, _r2 .* _l2, 'color', colour.orchid2, 'linestyle', ':');
+%%plot (x(1:3), _r2(1) .* _l2(1:3), 'r-');
+plot (x, _r2(1) .* _l2, 'r');
+plot ([x(1),x(1)], [_r2(2),_r2(1).*_l2(1)], 'ro-');
+plot (x(3:7), _r2(5) .* _l2(3:7), 'b');
+plot ([x(5),x(5)], [_r2(5),_r2(5).*_l2(5)], 'bo-');
+plot (x(7:11), _r2(9) .* _l2(7:11), 'g');
+plot ([x(9),x(9)], [_r2(9),_r2(9).*_l2(9)], 'go-');
+plot (x(11:15), _r2(13) .* _l2(11:15), 'm');
+plot ([x(13),x(13)], [_r2(13),_r2(13).*_l2(13)], 'mo-');
+plot (x(15:19), _r2(17) .* _l2(15:19), 'c');
+%%plot (x, _r2(17) .* _l2, 'k');
+plot ([x(17),x(17)], [_r2(17),_r2(17).*_l2(17)], 'co-');
+
+plot (x, _r2(21) .* _l2, 'k');
+plot ([x(21),x(21)], [_r2(21),_r2(21).*_l2(21)], 'ko-');
 
 lg = legend (['rcpt2';'lgnd2'],'Location','North');
 
 %% Now plot combined repulsion due to both receptor/ligand pairs
-plot_combined = 0
-if plot_combined
-  figure(1)
-  plot (x, _r2(1) .* _l1 + _r0(1) .* _l0, 'r--');
-  plot (x, _r2(5) .* _l1 + _r0(5) .* _l0, 'b--');
-  plot (x, _r2(9) .* _l1 + _r0(9) .* _l0, 'g--');
-  plot (x, _r2(13) .* _l1 + _r0(13) .* _l0, 'm--');
-  plot (x, _r2(17) .* _l1 + _r0(17) .* _l0, 'k--');
 
-  figure(2)
-  plot (x(1:3), _r2(1) .* _l1(1:3) + _r0(1) .* _l0(1:3), 'r--');
-  plot (x(3:7), _r2(5) .* _l1(3:7) + _r0(5) .* _l0(3:7), 'b--');
-  plot (x(7:11), _r2(9) .* _l1(7:11) + _r0(9) .* _l0(7:11), 'g--');
-  plot (x(11:15), _r2(13) .* _l1(11:15) + _r0(13) .* _l0(11:15), 'm--');
-  plot (x(15:19), _r2(17) .* _l1(15:19) + _r0(17) .* _l0(15:19), 'k--');
-end
+figure(3)
+clf; hold on;
+plot (x, _r2(1) .* _l2 + _r0(1) .* _l0, 'r-');
+plot (x, _r2(5) .* _l2 + _r0(5) .* _l0, 'b-');
+plot (x, _r2(9) .* _l2 + _r0(9) .* _l0, 'g-');
+plot (x, _r2(13) .* _l2 + _r0(13) .* _l0, 'm-');
+plot (x, _r2(17) .* _l2 + _r0(17) .* _l0, 'c-');
+plot (x, _r2(21) .* _l2 + _r0(21) .* _l0, 'k-');
+plot ([x(1),x(1)], [0,_r2(1).*_l2(1)+_r0(1).*_l0(1)], 'ro-');
+plot ([x(5),x(5)], [0,_r2(5).*_l2(5)+_r0(5).*_l0(5)], 'bo-');
+plot ([x(9),x(9)], [0,_r2(9).*_l2(9)+_r0(9).*_l0(9)], 'go-');
+plot ([x(13),x(13)], [0,_r2(13).*_l2(13)+_r0(13).*_l0(13)], 'mo-');
+plot ([x(17),x(17)], [0,_r2(17).*_l2(17)+_r0(17).*_l0(17)], 'co-');
+plot ([x(21),x(21)], [0,_r2(21).*_l2(21)+_r0(21).*_l0(21)], 'ko-');
+
+% figure(4)
+% clf; hold on;
+% plot (x(1:3), _r2(1) .* _l2(1:3) + _r0(1) .* _l0(1:3), 'r--');
+% plot (x(3:7), _r2(5) .* _l2(3:7) + _r0(5) .* _l0(3:7), 'b--');
+% plot (x(7:11), _r2(9) .* _l2(7:11) + _r0(9) .* _l0(7:11), 'g--');
+% plot (x(11:15), _r2(13) .* _l2(11:15) + _r0(13) .* _l0(11:15), 'm--');
+% plot (x(15:19), _r2(17) .* _l2(15:19) + _r0(17) .* _l0(15:19), 'c--');
+% plot (x(17:21), _r2(21) .* _l2(17:21) + _r0(21) .* _l0(17:21), 'k--');
+
 
 strlbl = ['Temporal -------------------------------------> Nasal'];
 figure(1)
 ylim([0,20])
 xlabel(strlbl)
 figure(2)
+ylim([0,20])
+xlabel(strlbl)
+figure(3)
 ylim([0,20])
 xlabel(strlbl)
