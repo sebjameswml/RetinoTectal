01af83adcfd572cbd4d1baf00934d33174d77a67
Seb James <seb.james@sheffield.ac.uk>
2022-02-23 09:54:08 +0000

    Rename script to follow previous naming convention

--- scripts/search_GJ.sh
+++ scripts/search_GJ.sh
@@ -6,4 +6,4 @@
 # GJ.json contains the starting model paramteres. s_GJ.json contains
 # the (simulated annealing) search parameters
 #
-./build/sim/agent/anneal1c ./optimization/m_GJ.json ./optimization/s_GJ.json
+./build/sim/agent/anneal1c ./optimization/m_ee_GJ.json ./optimization/s_GJ.json
