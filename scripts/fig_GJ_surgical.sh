b019f09f058d842123e2e4d824e58d711eafd4d2
Seb James <seb.james@sheffield.ac.uk>
2022-03-21 12:54:08 +0000

    Updates to GJ surgical

--- scripts/fig_GJ_surgical.sh
+++ scripts/fig_GJ_surgical.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot90_figcomp.json  -co:steps=1000
-./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot180_figcomp.json -co:steps=1000
-./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecswap_figcomp.json   -co:steps=1000
-./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_retablat_figcomp.json  -co:steps=1000
-./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecablat_figcomp.json  -co:steps=1000
-./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_mismatch_figcomp.json  -co:steps=1000
+./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot90_fig4row1.json -co:steps=1500
+./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot180_fig4row2.json  -co:steps=1500
+./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecswap_fig4row3.json  -co:steps=1500
+./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_retablat_fig4row4.json  -co:steps=1500
+./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecablat_fig4row5.json  -co:steps=1500
+./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_mismatch_fig4row6.json  -co:steps=1500
+
+./build/sim/agent/tissuevis configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot90_fig4row1.json
+./build/sim/agent/tissuevis configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot180_fig4row2.json
+./build/sim/agent/tissuevis configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecswap_fig4row3.json
+./build/sim/agent/tissuevis configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_retablat_fig4row4.json
+./build/sim/agent/tissuevis configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecablat_fig4row5.json
+./build/sim/agent/tissuevis configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_mismatch_fig4row6.json
+
+#./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot90_figcomp.json  -co:steps=1000
+#./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot180_figcomp.json -co:steps=1000
+#./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecswap_figcomp.json   -co:steps=1000
+#./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_retablat_figcomp.json  -co:steps=1000
+#./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecablat_figcomp.json  -co:steps=1000
+#./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_mismatch_figcomp.json  -co:steps=1000
 
 #./build/sim/agent/tissuevis configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot90_fig4row1.json  1000
 #./build/sim/agent/tissuevis configs/simpler/m_ee_GJ_best_1.json  configs/simpler/e_tecrot180_fig4row2.json  1000
