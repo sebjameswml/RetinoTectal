3922c462952e40c628f26d41c42692bc2aa961f6
Seb James <sjameswml@gmail.com>
2024-07-04 09:42:42 +0100

    Updates to images (following switch to HSV colour map

---
+++ paper/images/EphA_manipulations_readme.txt
@@ -0,0 +1,2 @@
+The third column of figures in EphA_manipulations is generated with
+the script analysis/bs_manip_vs_wt.py
--- scripts/paper_figures.sh
+++ scripts/paper_figures.sh
@@ -26,7 +26,10 @@ rm -rf ./rcnt/*.h5
 # 3. EphA3 ki/+ and EphA4 +/-
 ./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json configs/simpler/e_eph_ki-wt.json -co:exit=true -co:steps=1500
 ./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json configs/simpler/e_eph_kiki-wt.json -co:exit=true -co:steps=1500
+# e_eph_ki-kd_lite.json has less knockdown than e_eph_ki-kd.json (1.2 vs. 2.1 at time of writing)
 ./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json configs/simpler/e_eph_ki-kd_lite.json -co:exit=true -co:steps=1500
+
+./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1.json configs/simpler/e_eph_ki-kd.json -co:exit=true -co:steps=1500
 
 # Extended GJ model (EphA cluster size) plus the r2 collapse condition (EphA3 and EphA4 manipulations)
 ./build/sim/agent/agent1 configs/simpler/m_ee_GJ_best_1_EphA4_r2collapse.json configs/simpler/e_eph_ki-wt.json -co:exit=true -co:steps=1500
